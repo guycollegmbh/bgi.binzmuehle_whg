6ac3c61152a01777fe33990b9f1c59405a4fd41b
Patrick Grob <p.grob@guycolle.com>
2025-11-18 16:13:09 +0100

    Lang

--- src/Resources/contao/languages/en/tl_apartments.php
+++ src/Resources/contao/languages/en/tl_apartments.php
@@ -9,40 +9,37 @@ $GLOBALS['TL_LANG']['tl_apartments']['published_legend'] = 'Veröffentlichen';
 
 // Felder
 $GLOBALS['TL_LANG']['tl_apartments']['objektnummer'][0] = 'Objektnummer GP';
-$GLOBALS['TL_LANG']['tl_apartments']['objektnummer'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['objektnummer'][1] = 'Objektnummer der Wohnung eingeben';
 
 $GLOBALS['TL_LANG']['tl_apartments']['bezeichnung'][0] = 'Bezeichnung';
-$GLOBALS['TL_LANG']['tl_apartments']['bezeichnung'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['bezeichnung'][1] = 'Bezeichnung der Wohnung eingeben';
 
 $GLOBALS['TL_LANG']['tl_apartments']['bauetappe'][0] = 'Bauetappe';
-$GLOBALS['TL_LANG']['tl_apartments']['bauetappe'][1] = '';
-
-$GLOBALS['TL_LANG']['tl_apartments']['beschreibung'][0] = 'Beschreibung';
-$GLOBALS['TL_LANG']['tl_apartments']['beschreibung'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['bauetappe'][1] = 'Bauetappe auswählen';
 
 $GLOBALS['TL_LANG']['tl_apartments']['zeile'][0] = 'Zeile';
-$GLOBALS['TL_LANG']['tl_apartments']['zeile'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['zeile'][1] = 'Zeile auswählen';
 
 $GLOBALS['TL_LANG']['tl_apartments']['adresse'][0] = 'Adresse';
-$GLOBALS['TL_LANG']['tl_apartments']['adresse'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['adresse'][1] = 'Adresse auswählen';
 
 $GLOBALS['TL_LANG']['tl_apartments']['etage'][0] = 'Etage';
-$GLOBALS['TL_LANG']['tl_apartments']['etage'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['etage'][1] = 'Etage auswählen';
 
 $GLOBALS['TL_LANG']['tl_apartments']['zimmer'][0] = 'Zimmer';
-$GLOBALS['TL_LANG']['tl_apartments']['zimmer'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['zimmer'][1] = 'Anzahl Zimmer auswählen';
 
 $GLOBALS['TL_LANG']['tl_apartments']['flaeche'][0] = 'Fläche (m²)';
-$GLOBALS['TL_LANG']['tl_apartments']['flaeche'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['flaeche'][1] = 'Fläche ohne "(m²)"eingeben';
 
-$GLOBALS['TL_LANG']['tl_apartments']['nettomietzins'][0] = 'Nettomietzins ';
-$GLOBALS['TL_LANG']['tl_apartments']['nettomietzins'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['nettomietzins'][0] = 'Nettomietzins';
+$GLOBALS['TL_LANG']['tl_apartments']['nettomietzins'][1] = 'Nettomietzins ohne "CHF" und ".-" eingeben';
 
 $GLOBALS['TL_LANG']['tl_apartments']['nebenkosten'][0] = 'Nebenkosten';
-$GLOBALS['TL_LANG']['tl_apartments']['nebenkosten'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['nebenkosten'][1] = 'Nebenkosten ohne "CHF" und ".-" eingeben';
 
 $GLOBALS['TL_LANG']['tl_apartments']['bruttomietzins'][0] = 'Bruttomietzins';
-$GLOBALS['TL_LANG']['tl_apartments']['bruttomietzins'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['bruttomietzins'][1] = 'Bruttomietzins ohne "CHF" und ".-" eingeben';
 
 $GLOBALS['TL_LANG']['tl_apartments']['imagegrundriss'][0] = 'Visualisierung Grundriss';
 $GLOBALS['TL_LANG']['tl_apartments']['imagegrundriss'][1] = '';
@@ -53,5 +50,5 @@ $GLOBALS['TL_LANG']['tl_apartments']['imageetage'][1] = '';
 $GLOBALS['TL_LANG']['tl_apartments']['grundrisspdf'][0] = 'Grundriss PDF';
 $GLOBALS['TL_LANG']['tl_apartments']['grundrisspdf'][1] = '';
 
-$GLOBALS['TL_LANG']['tl_apartments']['published'][0] = 'Wohnung Veröffentlichen';
-$GLOBALS['TL_LANG']['tl_apartments']['published'][1] = '';
+$GLOBALS['TL_LANG']['tl_apartments']['published'][0] = 'Veröffentlichen';
+$GLOBALS['TL_LANG']['tl_apartments']['published'][1] = 'Wohnung Veröffentlichen';
