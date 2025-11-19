99edf6444f35d32db58526efd9831c3c9a60a119
Patrick Grob <p.grob@guycolle.com>
2025-11-19 12:57:16 +0100

    details

--- src/Module/ApartmentsDetailModule.php
+++ src/Module/ApartmentsDetailModule.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Guycollegmbh\ApartmentsBundle\Module;
 
 use Contao\Database;
+use Contao\FilesModel;
 use Contao\Module;
-use Contao\Environment;
 use Contao\Input;
 
 class ApartmentsDetailModule extends Module
@@ -15,7 +15,7 @@ class ApartmentsDetailModule extends Module
 
     protected function compile(): void
     {
-        // Hole die ID aus der URL (z.B. /wohnungen/detail/5)
+        // Hole die ID aus der URL
         $apartmentId = Input::get('id');
 
         if (!$apartmentId) {
@@ -36,7 +36,26 @@ class ApartmentsDetailModule extends Module
             return;
         }
 
-        $this->Template->apartment = $apartment->row();
+        $apartmentData = $apartment->row();
+
+        // Bilder vorbereiten
+        if ($apartmentData['imagegrundriss']) {
+            $file = FilesModel::findByUuid($apartmentData['imagegrundriss']);
+            $apartmentData['imagegrundriss_path'] = $file ? $file->path : null;
+        }
+
+        if ($apartmentData['imageetage']) {
+            $file = FilesModel::findByUuid($apartmentData['imageetage']);
+            $apartmentData['imageetage_path'] = $file ? $file->path : null;
+        }
+
+        if ($apartmentData['grundrisspdf']) {
+            $file = FilesModel::findByUuid($apartmentData['grundrisspdf']);
+            $apartmentData['grundrisspdf_path'] = $file ? $file->path : null;
+            $apartmentData['grundrisspdf_name'] = $file ? $file->name : 'Grundriss';
+        }
+
+        $this->Template->apartment = $apartmentData;
         $this->Template->error = null;
     }
 }
