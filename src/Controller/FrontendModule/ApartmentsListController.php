0872892e1db8b4b73afd86a946db6cc03b80cddc
Patrick Grob <p.grob@guycolle.com>
2025-11-19 11:38:25 +0100

    controller

--- src/Controller/FrontendModule/ApartmentsListController.php
+++ src/Controller/FrontendModule/ApartmentsListController.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace Guycollegmbh\ApartmentsBundle\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
-use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\Database;
 use Contao\ModuleModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsFrontendModule(category: 'apartments', template: 'mod_apartments_list')]
 class ApartmentsListController extends AbstractFrontendModuleController
 {
     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
