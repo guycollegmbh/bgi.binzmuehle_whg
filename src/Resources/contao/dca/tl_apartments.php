38b8a6eace3bfcbf0b8b247f2a770d96701f0701
Patrick Grob <p.grob@guycolle.com>
2025-11-21 14:55:19 +0100

    header

--- src/Resources/contao/dca/tl_apartments.php
+++ src/Resources/contao/dca/tl_apartments.php
@@ -42,9 +42,10 @@ $GLOBALS['TL_DCA']['tl_apartments'] = [
         ],
         'global_operations' => [
             'import' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_apartments']['import'],
                 'href'  => 'key=import',
-                'class' => 'header_import',
-                'icon'  => 'bundles/contaocore/images/upload.svg',
+                'class' => 'header_new',
+                'icon'  => 'new.svg',
                 'attributes' => 'onclick="Backend.getScrollOffset()"',
             ],
             'all' => [
