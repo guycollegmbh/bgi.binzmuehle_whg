42925894fc4f1615e05f7a6e5da1193df36e7545
Patrick Grob <p.grob@guycolle.com>
2025-12-03 16:46:48 +0100

    Listing Visuals

--- src/Module/ApartmentsListModule.php
+++ src/Module/ApartmentsListModule.php
@@ -6,8 +6,8 @@ namespace Guycollegmbh\ApartmentsBundle\Module;
 
 use Contao\Database;
 use Contao\Module;
-use Contao\Controller;
 use Contao\FilesModel;
+use Contao\Input;
 
 class ApartmentsListModule extends Module
 {
@@ -15,24 +15,47 @@ class ApartmentsListModule extends Module
 
     protected function compile(): void
     {
-        // Hole alle veröffentlichten Apartments
-        $apartments = Database::getInstance()
-            ->prepare('SELECT * FROM tl_apartments WHERE published = ? ORDER BY bauetappe, zeile, objektnummer')
-            ->execute(1);
+        $db = Database::getInstance();
 
-        $apartmentsList = [];
-        while ($apartments->next()) {
-            $data = $apartments->row();
-            
-            // HTML-Entities dekodieren
-            $objektnr = html_entity_decode($data['objektnummer'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-            // Entferne alles in Klammern
-            $objektnr = preg_replace('/\s*\([^)]*\)/', '', $objektnr);
-            // Entferne Punkte und Leerzeichen, behalte nur Zahlen
-            $objektnr = preg_replace('/[^0-9]/', '', $objektnr);
-            
-            $data['objektnummer_url'] = $objektnr;
-            
+        // Basis-Query
+        $query = 'SELECT * FROM tl_apartments WHERE published = ?';
+        $params = [1];
+
+        // Filter verarbeiten
+        $where = [];
+
+        if (Input::get('status')) {
+            $where[] = 'status = ?';
+            $params[] = Input::get('status');
+        }
+
+        if (Input::get('zimmer')) {
+            $where[] = 'zimmer = ?';
+            $params[] = Input::get('zimmer');
+        }
+
+        if (Input::get('bauetappe')) {
+            $where[] = 'bauetappe = ?';
+            $params[] = Input::get('bauetappe');
+        }
+
+        if (!empty($where)) {
+            $query .= ' AND ' . implode(' AND ', $where);
+        }
+
+        $query .= ' ORDER BY objektnummer ASC';
+
+        // Query ausführen
+        $result = $db->prepare($query)->execute(...$params);
+
+        // Apartments mit Pfaden
+        $apartments = [];
+        while ($result->next()) {
+            $data = $result->row();
+
+            // URL-freundliche Objektnummer
+            $data['objektnummer_url'] = urlencode($data['objektnummer']);
+
             // PDF-Pfad generieren falls vorhanden
             if ($data['grundrisspdf']) {
                 $fileModel = FilesModel::findByUuid($data['grundrisspdf']);
@@ -40,10 +63,38 @@ class ApartmentsListModule extends Module
                     $data['grundrisspdf_path'] = $fileModel->path;
                 }
             }
-            
-            $apartmentsList[] = $data;
+
+            // Grundriss-Bild Pfad generieren
+            if ($data['imagegrundriss']) {
+                $fileModel = FilesModel::findByUuid($data['imagegrundriss']);
+                if ($fileModel) {
+                    $data['imagegrundriss_path'] = $fileModel->path;
+                }
+            }
+
+            // Etagen-Bild Pfad generieren
+            if ($data['imageetage']) {
+                $fileModel = FilesModel::findByUuid($data['imageetage']);
+                if ($fileModel) {
+                    $data['imageetage_path'] = $fileModel->path;
+                }
+            }
+
+            $apartments[] = $data;
         }
 
-        $this->Template->apartments = $apartmentsList;
+        // Filter-Optionen
+        $statusOptions = $db->execute('SELECT DISTINCT status FROM tl_apartments WHERE published = 1 ORDER BY status')->fetchAllAssoc();
+        $zimmerOptions = $db->execute('SELECT DISTINCT zimmer FROM tl_apartments WHERE published = 1 ORDER BY zimmer')->fetchAllAssoc();
+        $bauetappeOptions = $db->execute('SELECT DISTINCT bauetappe FROM tl_apartments WHERE published = 1 ORDER BY bauetappe')->fetchAllAssoc();
+
+        // Template-Variablen
+        $this->Template->apartments = $apartments;
+        $this->Template->statusOptions = $statusOptions;
+        $this->Template->zimmerOptions = $zimmerOptions;
+        $this->Template->bauetappeOptions = $bauetappeOptions;
+        $this->Template->currentStatus = Input::get('status');
+        $this->Template->currentZimmer = Input::get('zimmer');
+        $this->Template->currentBauetappe = Input::get('bauetappe');
     }
 }
