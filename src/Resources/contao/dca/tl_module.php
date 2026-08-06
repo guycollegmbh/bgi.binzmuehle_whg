d6499bf628212ac7fad5078037cd7ab5276dfffd
Patrick Grob <p.grob@guycolle.com>
2026-08-06 16:48:07 +0200

    Vereinfache die Palette-Definition für 'apartments_list' und 'apartments_detail', entferne PaletteManipulator

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -2,16 +2,8 @@
 
 declare(strict_types=1);
 
-use Contao\CoreBundle\DataContainer\PaletteManipulator;
-
-PaletteManipulator::create()
-    ->addField('jumpTo', 'title_legend', PaletteManipulator::POSITION_APPEND)
-    ->addField('hide_bewerben', 'title_legend', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('apartments_list', 'tl_module');
-
-PaletteManipulator::create()
-    ->addField('hide_bewerben', 'title_legend', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('apartments_detail', 'tl_module');
+$GLOBALS['TL_DCA']['tl_module']['palettes']['apartments_list'] = '{title_legend},name,headline,type,jumpTo,hide_bewerben;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['apartments_detail'] = '{title_legend},name,headline,type,hide_bewerben;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['hide_bewerben'] = [
     'inputType' => 'checkbox',
